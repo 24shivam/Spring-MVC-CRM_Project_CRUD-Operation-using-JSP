71bf82e74ba56fc40b94e5ff0df7a29d3d824052
Shivam Dubey <dubeyshivam437@gmail.com>
2023-05-13 14:20:48 +0530

    COMMIT

---
+++ src/main/java/in/ineuron/SpringBootMvcCrmCrudAppJspApplication.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcCrmCrudAppJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcCrmCrudAppJspApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/ineuron/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package in.ineuron.service;
+
+import java.util.List;
+
+import in.ineuron.model.Customer;
+
+public interface ICustomerService {
+
+	public List<Customer> getCustomer();
+	
+	public void saveCustomer(Customer customer);
+	
+	public Customer getCustomer(Integer customerId);
+
+	public void deleteCustomer(Integer customerId);
+	
+}
